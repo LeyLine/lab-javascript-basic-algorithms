7686708c2571b7c30a2e4f6cc90208d3ba0b5ace
Adrian Llerena <a.llerena001@gmail.com>
2018-05-16 00:24:28 -0400

    Names and Input

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,5 +1,8 @@
 // Names and Input
-
+var driver1 = 'Adrian';
+var driver2= prompt("Who is the navigator?")
+console.log("The drivers name is "+ driver1);
+console.log("The drivers name is "+ driver2)
 
 //Conditionals
 
